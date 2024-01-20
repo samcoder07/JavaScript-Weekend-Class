00da9f3f3b1f396314a0c158dd7e6eeed665b874
samarthbluelupin <145488981+samcoder07@users.noreply.github.com>
2024-01-20 16:07:53 +0530

    checkin

---
+++ fundamental.js
@@ -0,0 +1,49 @@
+// code structure 
+
+// statement 
+console.log("hello world")
+// Semicolons
+console.log("hello world");
+// comments 
+/**
+ * console.log("hello world")
+ */
+
+/**
+ * Variales
+ *  A variable
+ * let 
+ * const 
+ * var 
+ * variale naming
+ * the name must contain only letters, digits,or the symbols $ and _.
+ * The first character must not be a digit.
+ */
+
+// var 
+/**
+ *  var a=10;
+a=20;
+console.log(a);
+ */
+
+// let 
+/**s
+ *  let a=10;
+let a=10;
+a=20;
+console.log(a);
+ */
+
+// const 
+/**
+ *  const PI= 3.14;
+ *  const PI= 3.14;
+console.log(PI);
+ */
+
+
+
+
+
+
